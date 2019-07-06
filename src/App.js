9902f659f0adec0d3d325e92a0c5fb0d2a5b688f
Michael Landers <landers.mike@gmail.com>
2019-07-06 08:49:45 -0700

    handles 1 preceeding 0

--- src/App.js
+++ src/App.js
@@ -5,33 +5,46 @@ import github from './github.png';
 import { checkNum } from './checkNum';
 
 function App() {
-    const [output, setOutput] = useState('');
-    const handleChange = e => {
-        e.preventDefault();
-        setOutput(checkNum(e.target.value));
-    };
-    return (
-        <div className="App">
-            <h1>Number to String!</h1>
-            <h3>Enter a number up to 999,999</h3>
-            <input
-                className="input"
-                type="number"
-                onChange={handleChange}
-                style={{ width: '100px', height: '25px' }}
-            />
+	const [output, setOutput] = useState('');
 
-            <div style={{ marginTop: '15px' }}>{output}</div>
+	const removeZero = num => {
+		return num.slice(1, num.length);
+	};
+	const handleChange = e => {
+		e.preventDefault();
+		let userNum = e.target.value;
+		if (userNum.length > 1 && userNum.charAt(0) === '0') {
+			userNum = removeZero(userNum);
+		}
 
-            <a href="https://github.com/mlanders/num_to_string">
-                <img
-                    src={github}
-                    alt="GitHub"
-                    style={{ width: '50px', marginTop: '15px' }}
-                />
-            </a>
-        </div>
-    );
+		// let y = x.toString();
+		// console.log(x);
+		// console.log(y);
+
+		setOutput(checkNum(userNum));
+	};
+	return (
+		<div className='App'>
+			<h1>Number to String!</h1>
+			<h3>Enter a number up to 999,999</h3>
+			<input
+				className='input'
+				type='number'
+				onChange={handleChange}
+				style={{ width: '100px', height: '25px' }}
+			/>
+
+			<div style={{ marginTop: '15px' }}>{output}</div>
+
+			<a href='https://github.com/mlanders/num_to_string'>
+				<img
+					src={github}
+					alt='GitHub'
+					style={{ width: '50px', marginTop: '15px' }}
+				/>
+			</a>
+		</div>
+	);
 }
 
 export default App;
