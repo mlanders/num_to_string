c2de3ca7e381b3a1c918a5ec72c9be60fbf89e6b
Mike Landers <landers.mike@gmail.com>
2020-04-06 08:28:34 -0700

    cleaned up code

--- src/App.js
+++ src/App.js
@@ -1,47 +1,38 @@
-import React, { useState } from 'react';
-import './App.css';
-import github from './github.png';
+import React, { useState } from "react";
+import "./App.css";
+import github from "./github.png";
 
-import { checkNum } from './checkNum';
+import { checkNum } from "./util";
 
 function App() {
-	const [output, setOutput] = useState('');
+	const [output, setOutput] = useState("");
 
-	const removeZero = num => {
+	const removeZero = (num) => {
 		return num.slice(1, num.length);
 	};
-	const handleChange = e => {
+	const handleChange = (e) => {
 		e.preventDefault();
 		let userNum = e.target.value;
-		if (userNum.length > 1 && userNum.charAt(0) === '0') {
+		if (userNum.length > 1 && userNum.charAt(0) === "0") {
 			userNum = removeZero(userNum);
 		}
-
-		// let y = x.toString();
-		// console.log(x);
-		// console.log(y);
-
 		setOutput(checkNum(userNum));
 	};
 	return (
-		<div className='App'>
+		<div className="App">
 			<h1>Number to String!</h1>
 			<h3>Enter a number up to 999,999</h3>
 			<input
-				className='input'
-				type='number'
+				className="input"
+				type="number"
 				onChange={handleChange}
-				style={{ width: '100px', height: '25px' }}
+				style={{ width: "100px", height: "25px" }}
 			/>
 
-			<div style={{ marginTop: '15px' }}>{output}</div>
+			<div style={{ marginTop: "15px" }}>{output}</div>
 
-			<a href='https://github.com/mlanders/num_to_string'>
-				<img
-					src={github}
-					alt='GitHub'
-					style={{ width: '50px', marginTop: '15px' }}
-				/>
+			<a href="https://github.com/mlanders/num_to_string">
+				<img src={github} alt="GitHub" style={{ width: "50px", marginTop: "15px" }} />
 			</a>
 		</div>
 	);
--- src/util.js
+++ src/util.js
@@ -1,23 +1,59 @@
-import { single, double, tens } from './nums';
+// import { single, double, tens } from './nums';
+const single = {
+	0: "Zero",
+	1: "One",
+	2: "Two",
+	3: "Three",
+	4: "Four",
+	5: "Five",
+	6: "Six",
+	7: "Seven",
+	8: "Eight",
+	9: "Nine",
+};
 
-/**
- *
- * @param {number} num
- */
-export const oneNum = num => {
-    return single[num];
+const double = {
+	10: "Ten",
+	11: "Eleven",
+	12: "Twelve",
+	13: "Thirteen",
+	14: "Fourteen",
+	15: "Fifteen",
+	16: "Sixteen",
+	17: "Seventeen",
+	18: "Eighteen",
+	19: "Nineteen",
+};
+
+const tens = {
+	2: "Twenty",
+	3: "Thirty",
+	4: "Fourty",
+	5: "Fifty",
+	6: "Sixty",
+	7: "Seventy",
+	8: "Eighty",
+	9: "Ninety",
 };
 
 /**
  *
  * @param {number} num
  */
-export const twoNum = num => {
-    if (Number(num[0]) !== 1) {
-        return `${tens[num[0]]} ${single[num[1]]}`;
-    } else {
-        return double[num];
-    }
+const oneNum = (num) => {
+	return single[num];
+};
+
+/**
+ *
+ * @param {number} num
+ */
+const twoNum = (num) => {
+	if (Number(num[0]) !== 1) {
+		return `${tens[num[0]]} ${single[num[1]]}`;
+	} else {
+		return double[num];
+	}
 };
 
 /**
@@ -25,8 +61,8 @@ export const twoNum = num => {
  * @param {number} tens
  * @param {number} hundreds
  */
-export const threeNum = (tens, hundreds) => {
-    return `${oneNum(hundreds)} Hundred ${twoNum(tens)}`;
+const threeNum = (tens, hundreds) => {
+	return `${oneNum(hundreds)} Hundred ${twoNum(tens)}`;
 };
 
 /**
@@ -35,8 +71,8 @@ export const threeNum = (tens, hundreds) => {
  * @param {number} hundreds
  * @param {number} thousand
  */
-export const fourNum = (tens, hundreds, thousand) => {
-    return `${oneNum(thousand[0])} Thousand ${threeNum(tens, hundreds)}`;
+const fourNum = (tens, hundreds, thousand) => {
+	return `${oneNum(thousand[0])} Thousand ${threeNum(tens, hundreds)}`;
 };
 
 /**
@@ -45,8 +81,8 @@ export const fourNum = (tens, hundreds, thousand) => {
  * @param {number} hundreds
  * @param {number} tenThousand
  */
-export const fiveNum = (tens, hundreds, tenThousand) => {
-    return `${twoNum(tenThousand)} Thousand ${threeNum(tens, hundreds)}`;
+const fiveNum = (tens, hundreds, tenThousand) => {
+	return `${twoNum(tenThousand)} Thousand ${threeNum(tens, hundreds)}`;
 };
 
 /**
@@ -55,8 +91,40 @@ export const fiveNum = (tens, hundreds, tenThousand) => {
  * @param {number} hundreds
  * @param {number} hundredThousand
  */
-export const sixNum = (tens, hundreds, hundredThousand) => {
-    const tens2 = hundredThousand.slice(-2);
-    const hundreds2 = hundredThousand.slice(-3, -2);
-    return `${threeNum(tens2, hundreds2)} Thousand ${threeNum(tens, hundreds)}`;
+const sixNum = (tens, hundreds, hundredThousand) => {
+	const tens2 = hundredThousand.slice(-2);
+	const hundreds2 = hundredThousand.slice(-3, -2);
+	return `${threeNum(tens2, hundreds2)} Thousand ${threeNum(tens, hundreds)}`;
+};
+
+/**
+ *
+ * @param {number} num
+ * @returns String version of the number
+ */
+export const checkNum = (num) => {
+	const length = num.length;
+	const single = num.slice(-1);
+	const tens = num.slice(-2);
+	const hundreds = num.slice(-3, -2);
+	const thousand = num.slice(-4, -3);
+	const tenThousand = num.slice(-5, -3);
+	const hundredThousand = num.slice(-6, -3);
+
+	switch (length) {
+		case 1: // Single
+			return oneNum(single);
+		case 2: // Ten
+			return twoNum(tens);
+		case 3: // Hundred
+			return threeNum(tens, hundreds);
+		case 4: // Thousaand
+			return fourNum(tens, hundreds, thousand);
+		case 5: // Ten Thousand
+			return fiveNum(tens, hundreds, tenThousand);
+		case 6: // Hundred Thousand
+			return sixNum(tens, hundreds, hundredThousand);
+		default:
+			return "";
+	}
 };
