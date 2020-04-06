9d1fe6c2cc120993aa916639cc5e4c9ad7bbf1c6
Mike Landers <landers.mike@gmail.com>
2020-04-06 08:44:27 -0700

    adds return app bar

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import github from "./github.png";
 
 import { checkNum } from "./util";
+import AppBar from "./AppBar";
 
 function App() {
 	const [output, setOutput] = useState("");
@@ -19,22 +20,25 @@ function App() {
 		setOutput(checkNum(userNum));
 	};
 	return (
-		<div className="App">
-			<h1>Number to String!</h1>
-			<h3>Enter a number up to 999,999</h3>
-			<input
-				className="input"
-				type="number"
-				onChange={handleChange}
-				style={{ width: "100px", height: "25px" }}
-			/>
+		<>
+			<AppBar />
+			<div className="App">
+				<h1>Number to String!</h1>
+				<h3>Enter a number up to 999,999</h3>
+				<input
+					className="input"
+					type="number"
+					onChange={handleChange}
+					style={{ width: "100px", height: "25px" }}
+				/>
 
-			<div style={{ marginTop: "15px" }}>{output}</div>
+				<div style={{ marginTop: "15px" }}>{output}</div>
 
-			<a href="https://github.com/mlanders/num_to_string">
-				<img src={github} alt="GitHub" style={{ width: "50px", marginTop: "15px" }} />
-			</a>
-		</div>
+				<a href="https://github.com/mlanders/num_to_string">
+					<img src={github} alt="GitHub" style={{ width: "50px", marginTop: "15px" }} />
+				</a>
+			</div>
+		</>
 	);
 }
 
